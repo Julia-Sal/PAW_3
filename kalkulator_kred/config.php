bc3f7eb1845e17c25a970b4ad12796e7acd00e04
Julia Sałacka <salacka.julia@gmail.com>
2022-03-21 19:25:02 +0100

    Config

--- kalkulator_kred/config.php
+++ kalkulator_kred/config.php
@@ -5,13 +5,4 @@ define('_APP_ROOT', '/kalkulator_kred');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
 
-<<<<<<< HEAD
 ?>
-=======
-function out(&$param){
-	if (isset($param)){
-		echo $param;
-	}
-}
-?>
->>>>>>> fd83b69a30277a949bd52bcdea636d891448196f
